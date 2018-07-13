ce942892af92377d282d31a6e593468f10e66a17
Alina Vanieva <vanieva.alina@yandex.ru>
2018-07-13 14:34:41 +0300

    adds mock data for news

---
+++ src/data/index.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        author: 'Sam',
+        preview: 'Text about Sam',
+        text: 'Very very very very very very text',
+    },
+    {
+        author: 'Bill',
+        preview: 'Text about Bill',
+        text: 'Very very very very very very text',
+    },
+    {
+        author: 'Sara',
+        preview: 'Text about Sara',
+        text: 'Very very very very very very text',
+    },
+];
