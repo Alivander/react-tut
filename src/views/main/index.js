11c4419e3d8f12b553e756d3d73f46629961713e
Alina Vanieva <vanieva.alina@yandex.ru>
2018-07-13 15:25:20 +0300

    adds main component

--- src/views/main/index.js
+++ src/views/main/index.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import b_ from 'b_';
+import Form from '../../components/form';
+import News from '../../components/news';
+import DataForNews from '../../data';
 import './index.css';
 
 const b = b_.with('main');
 
-function Main() {
-    return (
-        <div className={b()}>
-            <header className={b('header')}>
-                <h1 className={b('title')}>Sample SPA to React</h1>
-            </header>
-            <p className={b('welcom')}>
-                Welcom! :)
-            </p>
-        </div>
-    );
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            news: DataForNews,
+        };
+    }
+    addNews(news) {
+        this.setState({
+            news: this.state.news.slice().concat(news),
+        });
+    }
+    render() {
+        return (
+            <div className={b()}>
+                <header className={b('header')}>
+                    <h1 className={b('title')}>Sample SPA to React</h1>
+                </header>
+                <p className={b('welcom')}>
+                    Welcom! :)
+                </p>
+                <Form callback={news => this.addNews(news)} />
+                <News data={this.state.news} />
+            </div>
+        );
+    }
 }
 
 export default Main;
